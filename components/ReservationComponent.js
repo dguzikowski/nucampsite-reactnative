37a7298b06a12e08596f45b3bd358a94a2e0331b
david guzikowski <guzikowski.david@gmail.com>
2021-03-24 01:51:37 -0400

    Social Sharing

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Alert,
-    Picker, Switch, Button, Modal } from 'react-native';
+import { Text, View, ScrollView, StyleSheet,
+    Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import * as Animatable from 'react-native-animatable'; 
+import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -14,7 +15,6 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
         };
     }
 
@@ -22,29 +22,30 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    /*toggleModal() {
-        this.setState({showModal: !this.state.showModal});
-    }*/
 
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        let message = `Number of Campers: ${this.state.campers}\n\n` +
-        `Hike-In?: ${this.state.hikeIn}\n\n` +
-        `Date: ${this.state.date}`;
-
         Alert.alert(
+            // 1st param - title of the alert box
             'Begin Search?',
-            message,
+            // 2nd param - message shown in the dialog box
+            `Number of Campers: ${this.state.campers}\n\nHike-In: ${this.state.hikeIn}\n\nDate: ${this.state.date.toLocaleDateString('en-US')}`,
+            // 3rd param is a set of actions this needs to support provided as an array of objects - each is a button 
             [
                 {
                     text: 'Cancel',
-                    style: 'cancel',
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        this.resetForm();
+                    },
+                    style: 'cancel'
                 },
                 {
                     text: 'OK',
-                    style: 'cancel',
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
                 }
             ],
             { cancelable: false }
@@ -57,16 +58,40 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            //showModal: false
+            showModal: false
         });
     }
-    
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
 
     render() {
-       
         return (
             <ScrollView>
-                <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
+                <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
                     <View style={styles.formRow}>
                         <Text style={styles.formLabel}>Number of Campers</Text>
                         <Picker
@@ -115,14 +140,15 @@ class Reservation extends Component {
                     )}
                     <View style={styles.formRow}>
                         <Button
-                             onPress={() => {this.handleReservation()}}
+                            onPress={() => this.handleReservation()}
                             title='Search'
                             color='#5637DD'
                             accessibilityLabel='Tap me to search for available campsites to reserve'
                         />
                     </View>
+                    
                 </Animatable.View>
-         </ScrollView>
+            </ScrollView>
         );
     }
 }
@@ -141,22 +167,6 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
-    },
-    modal: { 
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
     }
 });
 
